d022545beee9429cdc930b8332ec8d3ed57c2fc0
Denis Falkowski <falkowski.denis@gmail.com>
2020-10-20 00:54:53 +0300

    feat: add solution for extended-repeater

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,7 +1,28 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function repeater(/* str, options */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+
+  let repeatTimes = options.repeatTimes || 0;
+  let separator = options.separator || '+';
+  let addition = String(options.addition) || '';
+  let additionRepeatTimes = options.additionRepeatTimes || 0;
+  let additionSeparator = options.additionSeparator || '|';
+  let result = '';
+
+
+  if (repeatTimes === 0 && additionRepeatTimes === 0)
+    result += str + addition;
+  else {
+    for (let i = 0; i < repeatTimes; i++) {
+      result += str;
+      for (let j = 0; j < additionRepeatTimes; j++) {
+        if (j < additionRepeatTimes - 1)
+         result += addition + additionSeparator;
+         else result += addition;
+      }
+      if (i < repeatTimes - 1) result += separator;
+    }
+  }
+  return result;
+
 };
-  
--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,66 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+/*   const doubleNext = '--double-next',
+        doublePrev = '--double-prev',
+        deleteNext = '--discard-next',
+        deletePrev = '--discard-prev';
+
+
+        if (!Array.isArray(arr)) { throw new Error('Error');}
+        let resArr = [];
+        
+        for (let i = 0; i < arr.length; i++) {
+          switch (arr[i]) {
+            
+            case doubleNext:
+            if(i  < arr.length - 1)  resArr.push(arr[i + 1]); 
+            break;
+      
+            case doublePrev:
+            if(i >= 1 && arr[i - 2] !== deleteNext){
+              resArr.push(arr[i - 1]);
+            }
+            break;
+      
+            case deleteNext:
+            i++;
+            break;
+      
+            case deletePrev:
+            if(i >= 1 && arr[i - 2] !== deleteNext){
+              resArr.splice(resArr.length - 1 , 1);
+            }
+            break;
+      
+            default:
+            resArr.push(arr[i]);
+          }
+        }
+        return resArr; */
+
+        if (!Array.isArray(arr)) {
+          throw new Error('Not array');
+        }
+      
+        let result = arr.slice();
+      
+        for (let i = 0; i < result.length; i++) {
+          if (result[i] == '--discard-next') {
+            result[i] = undefined;
+            result[i + 1] = undefined;
+            i += 1;
+          } else if (result[i] == '--discard-prev') {
+            result[i] = undefined;
+            result[i - 1] = undefined;
+          } else if (result[i] == '--double-next') {
+            result[i] = result[i + 1];
+          } else if (result[i] == '--double-prev') {
+            result[i] = result[i - 1];
+          } 
+        }
+      
+          return result.filter(item => item != undefined);
+      
+      
 };
